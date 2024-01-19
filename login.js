e3df798c7c98f0379c716a61771e83444184c389
gerardogarcia027 <156154521+gerardogarcia027@users.noreply.github.com>
2024-01-18 21:16:11 -0600

    login.js

--- login.js
+++ login.js
@@ -1,1 +1,7 @@
-
+document.addEventListener(´DOMContentLoaded´,Function(){
+  console.log("Hello, world!");
+  var inputEmail = document.getElementById(´email´);
+  var inputPassword = document.getElementById(´password´);
+  console.log(inputEmail);
+  console.log(inputPassword);
+});
